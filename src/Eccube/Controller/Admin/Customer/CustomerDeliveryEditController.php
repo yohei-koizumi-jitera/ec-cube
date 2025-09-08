4fd033866ee8cfeb2fcf383928892e8db2bd2d4b
yohei-koizumi-jitera <yohei.koizumi@jitera.com>
2025-09-08 18:06:33 +0900

    a

--- src/Eccube/Controller/Admin/Customer/CustomerDeliveryEditController.php
+++ src/Eccube/Controller/Admin/Customer/CustomerDeliveryEditController.php
@@ -1,17 +1,19 @@
 <?php
 
-/*
- * This file is part of EC-CUBE
- *
- * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
- *
- * http://www.ec-cube.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Eccube\Controller\Admin\Customer;
+
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Eccube\Controller\AbstractController;
+use Eccube\Entity\Customer;
+use Eccube\Entity\CustomerAddress;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
+use Eccube\Form\Type\Front\CustomerAddressType;
+use Eccube\Repository\CustomerAddressRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
 use Eccube\Controller\AbstractController;
@@ -28,7 +30,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerDeliveryEditController extends AbstractController
 {
-    protected CustomerAddressRepository $customerAddressRepository;
+    private CustomerAddressRepository $customerAddressRepository;
 
     public function __construct(CustomerAddressRepository $customerAddressRepository)
     {
@@ -45,48 +47,24 @@ class CustomerDeliveryEditController extends AbstractController
      */
     public function edit(Request $request, Customer $Customer, ?int $did = null)
     {
-        $CustomerAddress = $this->getCustomerAddressForEdit($Customer, $did);
+        $CustomerAddress = $this->resolveCustomerAddress($Customer, $did);
 
-        $builder = $this->formFactory->createBuilder(CustomerAddressType::class, $CustomerAddress);
-
-        $event = new EventArgs(
-            [
-                'builder' => $builder,
-                'Customer' => $Customer,
-                'CustomerAddress' => $CustomerAddress,
-            ],
-            $request
-        );
-        $this->eventDispatcher->dispatch($event, EccubeEvents::ADMIN_CUSTOMER_DELIVERY_EDIT_INDEX_INITIALIZE);
-
-        $form = $builder->getForm();
+        $form = $this->formFactory
+            ->createBuilder(CustomerAddressType::class, $CustomerAddress)
+            ->getForm();
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             log_info('お届け先登録開始', [$did]);
-
-            $this->entityManager->persist($CustomerAddress);
-            $this->entityManager->flush();
+            $this->persistAddress($CustomerAddress);
 
             log_info('お届け先登録完了', [$did]);
-
-            $event = new EventArgs(
-                [
-                    'form' => $form,
-                    'Customer' => $Customer,
-                    'CustomerAddress' => $CustomerAddress,
-                ],
-                $request
-            );
-            $this->eventDispatcher->dispatch($event, EccubeEvents::ADMIN_CUSTOMER_DELIVERY_EDIT_INDEX_COMPLETE);
-
             $this->addSuccess('admin.common.save_complete', 'admin');
+            $this->dispatchEvent(EccubeEvents::ADMIN_CUSTOMER_DELIVERY_EDIT_INDEX_COMPLETE, $form, $Customer, $CustomerAddress, $request);
 
-            return $this->redirect($this->generateUrl('admin_customer_delivery_edit', [
-                'id' => $Customer->getId(),
-                'did' => $CustomerAddress->getId(),
-            ]));
+            return $this->redirectToEdit($Customer, $CustomerAddress);
         }
+        $this->dispatchEvent(EccubeEvents::ADMIN_CUSTOMER_DELIVERY_EDIT_INDEX_INITIALIZE, null, $Customer, $CustomerAddress, $request);
 
         return [
             'form' => $form->createView(),
@@ -96,32 +74,60 @@ class CustomerDeliveryEditController extends AbstractController
     }
 
     /**
-     * お届け先情報取得処理（追加／編集）.
-     * 追加時は最大件数を超えていないかチェックも含む
+     * お届け先情報取得・最大件数制限対応
      */
-    private function getCustomerAddressForEdit(Customer $Customer, ?int $did): CustomerAddress
+    private function resolveCustomerAddress(Customer $Customer, ?int $did): CustomerAddress
     {
-        if (is_null($did)) {
-            // 新規追加
-            $addressCurrNum = count($Customer->getCustomerAddresses());
-            $addressMax = $this->eccubeConfig['eccube_deliv_addr_max'];
-            if ($addressCurrNum >= $addressMax) {
+        if ($did === null) {
+            if (count($Customer->getCustomerAddresses()) >= $this->eccubeConfig['eccube_deliv_addr_max']) {
                 throw new NotFoundHttpException('最大お届け先件数を超えています。');
             }
             $CustomerAddress = new CustomerAddress();
             $CustomerAddress->setCustomer($Customer);
-            return $CustomerAddress;
-        }
-
-        // 編集
-        $CustomerAddress = $this->customerAddressRepository->findOneBy([
-            'id' => $did,
-            'Customer' => $Customer,
-        ]);
-        if (!$CustomerAddress) {
-            throw new NotFoundHttpException('指定されたお届け先情報が存在しません。');
+        } else {
+            $CustomerAddress = $this->customerAddressRepository->findOneBy([
+                'id' => $did,
+                'Customer' => $Customer,
+            ]);
+            if (!$CustomerAddress) {
+                throw new NotFoundHttpException('指定されたお届け先情報が存在しません。');
+            }
         }
         return $CustomerAddress;
+    }
+
+    /**
+     * 共通：お届け先保存
+     */
+    private function persistAddress(CustomerAddress $address): void
+    {
+        $this->entityManager->persist($address);
+        $this->entityManager->flush();
+    }
+
+    /**
+     * 共通：イベントディスパッチ
+     */
+    private function dispatchEvent(string $eventName, $form, Customer $Customer, CustomerAddress $CustomerAddress, Request $request)
+    {
+        $data = [
+            'Customer' => $Customer,
+            'CustomerAddress' => $CustomerAddress
+        ];
+        if ($form !== null) $data['form'] = $form;
+        $event = new EventArgs($data, $request);
+        $this->eventDispatcher->dispatch($event, $eventName);
+    }
+
+    /**
+     * 共通：編集画面リダイレクト
+     */
+    private function redirectToEdit(Customer $Customer, CustomerAddress $CustomerAddress)
+    {
+        return $this->redirect($this->generateUrl('admin_customer_delivery_edit', [
+            'id' => $Customer->getId(),
+            'did' => $CustomerAddress->getId(),
+        ]));
     }
 
     /**
@@ -135,11 +141,7 @@ class CustomerDeliveryEditController extends AbstractController
         log_info('お届け先削除開始', [$did]);
 
         $CustomerAddress = $this->customerAddressRepository->find($did);
-        if ($CustomerAddress === null) {
-            throw new NotFoundHttpException('指定されたお届け先情報が存在しません。');
-        }
-
-        if ($CustomerAddress->getCustomer()->getId() !== $Customer->getId()) {
+        if (!$CustomerAddress || $CustomerAddress->getCustomer()->getId() !== $Customer->getId()) {
             $this->deleteMessage();
             return $this->redirect($this->generateUrl('admin_customer_edit', ['id' => $Customer->getId()]));
         }
@@ -154,15 +156,7 @@ class CustomerDeliveryEditController extends AbstractController
         }
 
         log_info('お届け先削除完了', [$did]);
-
-        $event = new EventArgs(
-            [
-                'Customer' => $Customer,
-                'CustomerAddress' => $CustomerAddress,
-            ],
-            $request
-        );
-        $this->eventDispatcher->dispatch($event, EccubeEvents::ADMIN_CUSTOMER_DELIVERY_DELETE_COMPLETE);
+        $this->dispatchEvent(EccubeEvents::ADMIN_CUSTOMER_DELIVERY_DELETE_COMPLETE, null, $Customer, $CustomerAddress, $request);
 
         return $this->redirect($this->generateUrl('admin_customer_edit', ['id' => $Customer->getId()]));
     }
